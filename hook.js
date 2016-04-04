2c9bf62d66ec504a748e2789f90fc03c8714d645
Krish Joshi <me@krishj.com>
2016-04-04 23:55:15 +0100

    hook update

--- hook.js
+++ hook.js
@@ -1,15 +1,15 @@
 var gith = require('gith').create(6000);  // run on port 6000
 var exec = require('child_process').exec; 
 
-console.log("started");
-console.log(gith);
-
 gith({
   repo: 'KrishJoshi/ParseSite'  // the github-user/repo-name
 }).on('all', function(payload){
     console.log("push received");
     exec('hook.sh ' + payload.branch, function(err, stdout, stderr){
-      if (err) return err;
+      if (err) {
+      	console.log(err);
+      	return err;
+      }
       console.log(stdout);
       console.log("git deployed to branch " + payload.branch);
     });
